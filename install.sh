628c1b2f3126eb171eee2891965d5d577c93b4b2
Jonathas-Conceicao <jadoliveira@inf.ufpel.edu.br>
2017-04-01 20:35:13 -0300

    ops!

--- install.sh
+++ install.sh
@@ -50,7 +50,7 @@ echo "## Installing ##"
 if [ ! -d "$JAR_LOCATION"/.Jar ]; then
   echo "Creating directory for program's files"
   mkdir --parent "$JAR_LOCATION"/.Jar
-  mkdir 666 $JAR_LOCATION
+  chmod 666 $JAR_LOCATION
 fi
 if [ ! -d "$BIN_LOCATION" ]; then
   echo "Creating directory for program's binary"
